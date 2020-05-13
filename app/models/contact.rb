dfc932092125d5b1c2f2bb91c89bc75451b01734
seu nome <fabioolivc@gmail.com>
2020-05-13 12:43:04 -0300

    Application api

---
+++ app/models/contact.rb
@@ -0,0 +1,33 @@
+class Contact < ApplicationRecord
+  belongs_to :kind, optional: false
+  has_many :phones
+
+  def to_br
+    {
+      name: self.name, 
+      email:self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.blank?)
+    }
+  end
+  #def birthdate_br
+  #  I18n.l(self.birthdate) unless self.birthdate.blank?
+  #end
+  #def author
+  #  "Fábio Coelho"
+  #end
+
+  #def kind_description
+  #  self.kind.description
+  #end
+
+  #def as_json(options={})
+  #  super(
+  #    root: true,
+  #    methods: [:kind_description, :author],
+  #    include: { kind: { only: :description}}
+  #  )
+  #end
+  #def hello
+  #  I18n.t('hello')
+  #end
+end
